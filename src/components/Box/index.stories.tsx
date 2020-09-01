41d699adeb50968805296fb3d41b5d39ef957318
Sergio Júnior <sergioamjr91@gmail.com>
2020-09-01 19:30:10 -0300

    Setting box position and size

--- src/components/Box/index.stories.tsx
+++ src/components/Box/index.stories.tsx
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import Box from "./";
+import { Size, Position } from "~types";
 
 export default {
   title: "Box",
@@ -7,14 +8,20 @@ export default {
 };
 
 export const Primary = () => {
-  const onDragStop = () => {};
-  const onResize = () => {};
+  const [position, setPosition] = useState<Position>({ x: 10, y: 20 });
+  const [size, setSize] = useState<Size>({ width: 200, height: 200 });
+  const onDragStop = (e, { x, y }) => {
+    setPosition({ x, y });
+  };
+  const onResize = (e, direction, ref) => {
+    setSize({ width: ref.style.width, height: ref.style.height });
+  };
   return (
     <Box
       onResize={onResize}
       onDragStop={onDragStop}
-      size={{ width: 200, height: 200 }}
-      position={{ x: 10, y: 20 }}
+      size={size}
+      position={position}
     />
   );
 };
--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -1,17 +1,7 @@
 import React from "react";
 import { Rnd, DraggableData, RndResizeCallback } from "react-rnd";
 import * as S from "./style";
-
-type Size = { width: number; height: number };
-type Position = { x: number; y: number };
-type DraggableEventHandler = (e: any, data: DraggableData) => void | false;
-
-export type BoxProps = {
-  size: Size;
-  position: Position;
-  onDragStop: DraggableEventHandler;
-  onResize: RndResizeCallback;
-};
+import { BoxProps } from "~types";
 
 const Box = (props: BoxProps) => (
   <Rnd {...props}>
