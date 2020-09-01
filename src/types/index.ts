4bcb7d5a5f9d8f50430161892d99ce75382fa44f
Sergio Júnior <sergioamjr91@gmail.com>
2020-09-01 19:30:00 -0300

    Created alias

---
+++ src/types/index.ts
@@ -0,0 +1,15 @@
+import { DraggableData, RndResizeCallback } from "react-rnd";
+
+export type Size = { width: number; height: number };
+export type Position = { x: number; y: number };
+export type DraggableEventHandler = (
+  e: any,
+  data: DraggableData
+) => void | false;
+
+export type BoxProps = {
+  size: Size;
+  position: Position;
+  onDragStop: DraggableEventHandler;
+  onResize: RndResizeCallback;
+};
